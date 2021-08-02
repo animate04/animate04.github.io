86099585d589b6b7eed663f5424608228b1bfd2c
animate04 <71991008+animate04@users.noreply.github.com>
2021-08-02 10:59:57 -0300

    inicial

---
+++ wp-content/plugins/indeed-affiliate-pro/assets/js/public-upload-image.js
@@ -0,0 +1,15 @@
+jQuery( document ).ready( function(){
+    jQuery( '.uap-js-public-upload-image-data' ).each(function(e,html){
+      UapAvatarCroppic.init({
+          triggerId					           : jQuery( html ).attr( 'data-trigger_id' ),
+          saveImageTarget		           : jQuery( html ).attr( 'data-save_image_target' ),
+          cropImageTarget              : jQuery( html ).attr( 'data-crop_image_target' ),
+          imageSelectorWrapper         : '.uap-upload-image-wrapp',
+          hiddenInputSelector          : '[name=' + jQuery( html ).attr( 'data-name' ) + ']',
+          imageClass                   : 'uap-member-photo',
+          removeImageSelector          : '#uap_upload_image_remove_bttn_' + jQuery( html ).attr( 'data-remove_selector' ),
+          buttonId 					           : 'uap-avatar-button',
+          buttonLabel 			           : jQuery( html ).attr( 'data-button_label' ),
+      });
+    });
+});
